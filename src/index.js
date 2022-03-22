60dd9e42879e347fd88815396720fa8313b37ddb
Alexk <morfem95@gmail.com>
2022-03-22 15:47:14 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-let a = 100;
+const a = 100;
 outer:
 for (let b = 2; b <= a; b++){
     for (let c = 2; c < b; c++){
-        if (b % c == 0) continue outer;
+        if (b % c === 0) continue outer;
     }
     console.log(b);
 }
